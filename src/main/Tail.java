3edfcd3f4ce6c89b12814031c8be262e9579cb81
denis-shvetcov <deniska28082001@gmail.com>
2020-03-03 23:46:37 +0700

    03.03

--- src/main/Tail.java
+++ src/main/Tail.java
@@ -5,34 +5,33 @@ import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Tail {
     @Option(name = "-o", metaVar = "outputFile", usage = "output file")
     private String output;
 
     @Option(name = "-c", metaVar = "charNumber", usage = "the number of chars")
-    private int charN;
+    private Integer charN;
 
     @Option(name = "-n", metaVar = "stringNumber", usage = "the number of chars")
-    private int stringN = 10;
+    private Integer stringN;
 
 
-//    private List<String> files = new ArrayList<>();
-    @Argument
-    private  String files;
+    @Argument(metaVar = "inputFiles", usage = "files we copy from")
+    private List<String> files = new ArrayList<>();
 
 
-    public static void main(String[] args) throws IOException {
-        if (args.length>0) new Tail().launch(args);
+    public static void main(String[] args) {
+        if (args.length > 0) new Tail().launch(args);
     }
 
-    public void launch(String[] args) throws IOException {
+    public void launch(String[] args) {
         CmdLineParser parser = new CmdLineParser(this);
 
         try {
@@ -44,27 +43,99 @@ public class Tail {
             return;
         }
 
-        if (output!=null) {
-            System.out.println("got output");
+        if (charN != null && stringN != null) {
+            System.err.println("charN and stringN were entered at the same time");
         }
 
-        if (Integer.valueOf(charN)!=null) {
-            System.out.println(charN);
+        if (charN != null) {
+            if (output != null) {
+                toFileLastChars(charN, files, output);
+            } else {
+                toConsoleLastChars(charN, files);
+            }
         }
-
-        if (Integer.valueOf(stringN)!=null) {
-            System.out.println(stringN);
-        }
-
-//       try (BufferedWriter buff= Files.newBufferedWriter(Paths.get(files))){
-//           buff.write("sup dude");
-//       }
-
-        System.out.println(files);
-
 
 
     }
+
+
+    // Символы читаются в файл output.
+    public void toFileLastChars(int num, List<String> files, String output) {
+
+        try (BufferedWriter to = Files.newBufferedWriter(Paths.get(output))) {
+
+            if (files.size() != 0) {
+                for (int j = 0; j < files.size(); j++) {
+
+                    RandomAccessFile from = new RandomAccessFile(new File(files.get(j)), "r");
+
+                    if (files.size() > 1) {
+                        to.write(new File(files.get(j)).getName());
+                        to.newLine();
+                    }
+
+                    from.seek(from.length() - num);
+
+                    for (int i = (int) from.length() - num; i < from.length(); i++) {
+                        to.write((char) from.read());
+                    }
+                    to.newLine();
+                    from.close();
+                }
+            } else {
+                //Если нет файлов, из которых мы берем символы, то читаем num символов из консоли
+                Scanner scan = new Scanner(System.in);
+                String str = scan.nextLine(); // читаем введенный текст
+                scan.close();
+                //если длина str больше, чем num, то записываем все символы, иначе записываем весь str
+                to.write(str.substring(str.length() > num ? str.length() - num : 0));
+            }
+
+        } catch (FileNotFoundException exc) {
+            System.out.println("File doesn't exist");
+
+        } catch (IOException exc) {
+            System.out.println("Problems with reading, writing or closing files.");
+        }
+
+    }
+
+    //Символы читаются в консоль.
+    public void toConsoleLastChars(int num, List<String> files) {
+
+        try {
+            if (files.size() != 0) {
+                for (int j = 0; j < files.size(); j++) {
+
+                    RandomAccessFile from = new RandomAccessFile(new File(files.get(j)), "r");
+
+                    if (files.size() > 1) {
+                        System.out.println(new File(files.get(j)).getName());
+                    }
+
+                    from.seek(from.length() - num);
+
+                    for (int i = (int) from.length() - num; i < from.length(); i++) {
+                        System.out.print((char) from.read());
+                    }
+                    System.out.println();
+                    from.close();
+                }
+            } else {
+                //Если нет файлов, из которых мы берем символы, то читаем num символов из консоли
+                Scanner scan = new Scanner(System.in);
+                String str = scan.nextLine(); // читаем введенный текст
+                scan.close();
+                //если длина str больше, чем num, то записываем все символы, иначе записываем весь str
+                System.out.println(str.substring(str.length() > num ? str.length() - num : 0));
+            }
+
+        } catch (IOException exc) {
+            System.out.println("Problems with reading, writing or closing files.");
+        }
+
+    }
+
 
 }
 
